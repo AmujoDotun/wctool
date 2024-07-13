3395577eaf94bc5efd99b0a27b4c4f90d5359c0e
AmujoDotun <samueleamujo@gmail.com>
2024-07-13 18:14:00 -0500

    created a function to count bytes

---
+++ countbytes.py
@@ -0,0 +1,11 @@
+#Function to Count bytes
+import sys
+import os
+
+def count_byte(file_path):
+    try:
+        with open(file_path, 'rb') as file:
+            data = file.read()
+            print(f" {len(data)} {file_path}")
+    except FileNotFoundError:
+        print(f" File {file_path} not found.")
