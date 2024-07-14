a98115cdb54b27393a97bf907d3509800f58e11c
AmujoDotun <samueleamujo@gmail.com>
2024-07-14 16:36:16 -0500

    updated script to run countline function

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import sys
+from countbytes import count_bytes
+from lines import count_lines
+from characters import count_chars
 
-
-if __name__ == "__main__ ":
+if __name__ == "__main__":
+    print(f"Arguments received: {sys.argv}")
     if len(sys.argv) != 3 or sys.argv[1] not in ['-c', '-l']:
-        print("Usage: ccwc -c <file> or ccwc -l <file>")
-    elif sys.argv[1] == '-c':
-        count_bytes(sys.argv[2])
-    elif sys.argv[1] == '-l':
-        count_lines(sys.argv[2])
+        print("Usage: main.py -c <file> or main.py -l <file>")
+    else:
+        option = sys.argv[1]
+        file_path = sys.argv[2]
+        print(f"Option selected: {option}")
+        print(f"File path: {file_path}")
+
+        if option == '-c':
+            count_bytes(file_path)
+        elif option == '-l':
+            count_lines(file_path)
