ec388c1e6017373ce32d3190d8341eeefe821981
AmujoDotun <samueleamujo@gmail.com>
2024-07-14 16:47:04 -0500

    updated main file to run count_char function

--- main.py
+++ main.py
@@ -5,15 +5,13 @@ from characters import count_chars
 
 if __name__ == "__main__":
     print(f"Arguments received: {sys.argv}")
-    if len(sys.argv) != 3 or sys.argv[1] not in ['-c', '-l']:
-        print("Usage: main.py -c <file> or main.py -l <file>")
-    else:
-        option = sys.argv[1]
-        file_path = sys.argv[2]
-        print(f"Option selected: {option}")
-        print(f"File path: {file_path}")
-
-        if option == '-c':
-            count_bytes(file_path)
-        elif option == '-l':
-            count_lines(file_path)
+    if len(sys.argv) != 3 or sys.argv[1] not in ['-c', '-l', '-w', '-m']:
+        print("Usage: main.py -c <file>, main.py -l <file>, main.py -w <file>, main.py -m <file>")
+    elif sys.argv[1] == '-c':
+        count_bytes(sys.argv[2])
+    elif sys.argv[1] == '-l':
+        count_lines(sys.argv[2])
+    elif sys.argv[1] == '-w':
+        count_words(sys.argv[2])
+    elif sys.argv[1] == '-m':
+        count_chars(sys.argv[2])
